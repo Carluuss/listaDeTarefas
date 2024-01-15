eda68ae9b87dff0d3d32bd5abf952f732627e73d
Carlos <128008806+Carluuss@users.noreply.github.com>
2024-01-14 21:13:49 -0300

    terceiro commit

--- script.js
+++ script.js
@@ -1,13 +1,26 @@
 function adicionarTarefa(DoN) {
-    let tarefa = document.getElementById("tarefa");
-    let lista = document.getElementById("lista" + DoN);
-    let item = document.createElement("li");
-    item.innerHTML = `<span   class="marcacao" onclick="tarefaConcluida()">${tarefa.value}<span/>`;
-    lista.appendChild(item)
+   
+    if (tarefa.value.trim()==""){
+        alert("Digite uma tarefa a ser adicionada");
+    }else{
+        let tarefa = document.getElementById("tarefa");
+        let lista = document.getElementById("lista" + DoN);
+        let item = document.createElement("li");
+        item.innerHTML = `<span  ondblclick="tarefaIncompleta(this)" onclick="tarefaConcluida(this)">${tarefa.value}</span><button type='button' class="btnRemover" onclick="remover(this.parentNode)">Remover</button>`;
+        lista.appendChild(item)
+    }
 }
 
-function tarefaConcluida(){
-    let p = document.querySelector("span");
-    p.style.textDecoration = 'line-through';
-    p.style.color = 'red';
+function tarefaConcluida(span){
+    span.style.textDecoration = 'line-through';
+    span.style.color = 'red';
+}
+
+function tarefaIncompleta(span){
+    span.style.textDecoration = 'none';
+    span.style.color = 'white';
+}
+
+function remover(item){
+    item.parentNode.removeChild(item);
 }
